113db27861bce5549bf4c3b4fc6f9ff744535577
DuyLP <leduy1159@gmail.com>
2025-04-25 16:07:35 +0700

    Add EmailConfig & RedisConfig following with Region in each Config for more readable source

--- Application/Commons/AppConfiguration.cs
+++ Application/Commons/AppConfiguration.cs
@@ -4,14 +4,19 @@
     {
         public DatabaseConfig DatabaseConfig { get; set; } = null!;
         public JwtConfig JwtConfig { get; set; } = null!;
+        public EmailConfig EmailConfig { get; set; } = null!;
+        public RedisConfig RedisConfig { get; set; } = null!;
     }
 
+    #region DatabaseConfig
     public class DatabaseConfig
     {
         public string ConnectionString { get; set; } = string.Empty;
         public bool IsMemoryDB { get; set; } = false;
     }
+    #endregion
 
+    #region JwtConfig
     public class JwtConfig
     {
         public string SecretKey { get; set; } = string.Empty;
@@ -19,10 +24,21 @@
         public string Issuer { get; set; } = string.Empty;
         public string Audience { get; set; } = string.Empty;
     }
+    #endregion
 
+    #region EmailConfig
     public class EmailConfig
     {
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
+    #endregion
+
+    #region RedisConfig
+    public class RedisConfig
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string InstanceName { get; set; } = string.Empty;
+    }
+    #endregion
 }
