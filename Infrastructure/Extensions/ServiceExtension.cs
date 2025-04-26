61a410554a1e5a088b22ece10ec0a1afdd003a62
DuyLP <leduy1159@gmail.com>
2025-04-26 13:26:43 +0700

    Prevent the misconduct where WebAPI know about Domain

--- Infrastructure/Extensions/ServiceExtension.cs
+++ Infrastructure/Extensions/ServiceExtension.cs
@@ -1,6 +1,4 @@
-﻿using Application.Interfaces.Base;
-using Application.Interfaces.Services;
-using Infrastructure.Implements.Base;
+﻿using Application.Interfaces.Services;
 using Infrastructure.Implements.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,8 +8,6 @@ namespace Infrastructure.Extensions
     {
         public static IServiceCollection AddServiceScoped(this IServiceCollection services)
         {
-            services.AddScoped(typeof(IGetService<,>), typeof(GetService<,>));
-            services.AddScoped(typeof(ICrudService<,,,>), typeof(CrudService<,,,>));
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IProductService, ProductService>();
             return services;
